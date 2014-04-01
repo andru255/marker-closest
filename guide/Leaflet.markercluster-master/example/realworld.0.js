b0a4220f4729c75ecc7266b47a0eb526810cf61d
andru255 <andru255@gmail.com>
2014-04-01 00:21:57 -0500

    testing projection

---
+++ guide/Leaflet.markercluster-master/example/realworld.0.js
@@ -0,0 +1,10 @@
+
+//An extract of address points from the LINZ bulk extract: http://www.linz.govt.nz/survey-titles/landonline-data/landonline-bde
+//Should be this data set: http://data.linz.govt.nz/#/layer/779-nz-street-address-electoral/
+var addressPoints = [
+[-0.0000000000, 0.0000000000, "2"],
+[-0.0000000001, 0.0000000001, "3"],
+[-0.0000000010, 0.0000000010, "3A"],
+[-0.0000000100, 0.0000000100, "1"],
+[-0.0000001000, 0.0000001000, "5"]
+];
